9118bd7ea58375a68dc34d8ea99790bcf8f5fa9d
nataliamoran <natalja.moran@gmail.com>
2022-04-24 11:45:28 -0400

    Move translate button to a separate class

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 // import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'translate_button.dart';
 
 void main() {
   runApp(const MyApp());
@@ -114,50 +115,7 @@ class _MahzehState extends State<Mahzeh> {
                     overflow: TextOverflow.ellipsis,
                   ),
                 ),
-                Container(
-                    margin: const EdgeInsets.only(top: 5.0, bottom: 10.0),
-                    child: TextButton(
-                      child: Row(mainAxisSize: MainAxisSize.min, children: [
-                        Stack(
-                          children: <Widget>[
-                            const IconButton(
-                              icon: Icon(
-                                Icons.edit_note,
-                                color: Colors.black,
-                                size: 30.0,
-                              ),
-                              onPressed: null,
-                            ),
-                            _testAnswers.isEmpty
-                                ? Container()
-                                : Positioned(
-                                    bottom: 25.0,
-                                    child: Stack(
-                                      children: <Widget>[
-                                        const Icon(Icons.brightness_1,
-                                            size: 25.0,
-                                            color: Colors.deepOrange),
-                                        Positioned(
-                                            top: 5.0,
-                                            left: 8.0,
-                                            child: Center(
-                                              child: Text(
-                                                _testAnswers.length.toString(),
-                                                style: const TextStyle(
-                                                    color: Colors.white,
-                                                    fontSize: 12.0,
-                                                    fontWeight:
-                                                        FontWeight.w500),
-                                              ),
-                                            )),
-                                      ],
-                                    )),
-                          ],
-                        ),
-                        const Text('перекласти / перевести'),
-                      ]),
-                      onPressed: () {},
-                    )),
+                const TranslateButton(),
 
                 // Image.asset(_flowers[_flower_i % _flowers.length]),
                 // SizedBox(
--- lib/translate_button.dart
+++ lib/translate_button.dart
@@ -4,9 +4,52 @@ class _TranslateButtonState extends State<TranslateButton> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemBuilder: (context, i) {return const Divider();}
-    );
+    final _testAnswers = ["один тестовый ответ", "другой тестовый ответ"];
+
+    return Container(
+        margin: const EdgeInsets.only(top: 5.0, bottom: 10.0),
+        child: TextButton(
+          child: Row(mainAxisSize: MainAxisSize.min, children: [
+            Stack(
+              children: <Widget>[
+                const IconButton(
+                  icon: Icon(
+                    Icons.edit_note,
+                    color: Colors.black,
+                    size: 30.0,
+                  ),
+                  onPressed: null,
+                ),
+                _testAnswers.isEmpty
+                    ? Container()
+                    : Positioned(
+                    bottom: 25.0,
+                    child: Stack(
+                      children: <Widget>[
+                        const Icon(Icons.brightness_1,
+                            size: 25.0,
+                            color: Colors.deepOrange),
+                        Positioned(
+                            top: 5.0,
+                            left: 8.0,
+                            child: Center(
+                              child: Text(
+                                _testAnswers.length.toString(),
+                                style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 12.0,
+                                    fontWeight:
+                                    FontWeight.w500),
+                              ),
+                            )),
+                      ],
+                    )),
+              ],
+            ),
+            const Text('перекласти / перевести'),
+          ]),
+          onPressed: () {},
+        ));
   }
 }
 
