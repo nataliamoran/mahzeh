df80ce31a3cc5d0f010d29f472422b7297d3e4ae
nataliamoran <natalja.moran@gmail.com>
2022-05-19 13:14:00 -0300

    Add db in docker

--- mahzeh_backend/backend/backend/settings.py
+++ mahzeh_backend/backend/backend/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mahzehdb',
+        'USER': 'mahzehadmin',
+        'PASSWORD': 'mahzehpass1111',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
