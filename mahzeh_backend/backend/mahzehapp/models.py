12d0a3153876e0d31f174cc13921c9bad36136a0
nataliamoran <natalja.moran@gmail.com>
2022-05-22 15:50:22 -0300

    update models

--- mahzeh_backend/backend/mahzehapp/models.py
+++ mahzeh_backend/backend/mahzehapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    image = models.ImageField(upload_to='media/', blank=False, null=False)
+    description = models.TextField(blank=True)
+    is_ukrainian_lang = models.BooleanField(default=True)
+    is_russian_lang = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Answer(models.Model):
+    content = models.TextField(blank=False, max_length=500)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, blank=False, null=False, related_name='+')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
